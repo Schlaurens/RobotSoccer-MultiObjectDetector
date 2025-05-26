27bc393bcc056cf569ac0c4acce23867dcdf2384
Laurens Schiefelbein <laurens@uni-bremen.de>
2025-05-26 12:25:03 +0200

    also draw loss mask

--- util/image.py
+++ util/image.py
@@ -64,7 +64,7 @@ def show_cell_on_image(directory, label, object_name=None, grid_dims=(15, 20)):
     """Show the given image with an illustrated cell grid of given dimension.
 
     If an object_name is given and that object is present in the label. Its objectness
-    mask is drawn on the image.
+    mask and loss mask are drawn on the image.
 
     Args:
         directory: Directory of the image
@@ -90,29 +90,47 @@ def show_cell_on_image(directory, label, object_name=None, grid_dims=(15, 20)):
 
     # Draw the cell with the highest objectness score (if a valid object_name is given)
     if object_name in label:
-        _, objectness = u_dataset.get_masks(label, object_name, output_dims=grid_dims)
+        _, objectness_mask, loss_mask = u_dataset.get_masks(label, object_name, output_dims=grid_dims)
 
-        objectness = np.array(objectness)
+        objectness_mask = np.array(objectness_mask)
+        
         # Get the index with the highest value.
-        # TODO: correct when there are objects that cover more than
-        # one cell.
-        index = np.unravel_index(objectness.argmax(), objectness.shape)
+        indices_objectness = np.unravel_index(objectness_mask.argmax(), objectness_mask.shape)
+        indices_loss_mask = np.dstack(np.where(loss_mask == loss_mask.min()))[0]
 
         # scale the index to the size of the cell grid
-        scaled_index = index[::-1] * np.array(cell_dims)
+        scaled_objectness_mask_indices = indices_objectness * np.array(cell_dims)
+        scaled_loss_mask_indices = indices_loss_mask * np.array(cell_dims)
 
+        # Make sure that the indices are 2D arrays to make iteration possible in the next step.
+        if (len(scaled_loss_mask_indices.shape) == 1):
+            scaled_loss_mask_indices = np.expand_dims(scaled_loss_mask_indices, axis=0)
+        if (len(scaled_objectness_mask_indices.shape) == 1):
+            scaled_objectness_mask_indices = np.expand_dims(scaled_objectness_mask_indices, axis=0)
+        
         # Draw a rectangle on the cell that cover the object.
         # TODO: will need modification when there
         # are multiple cell that cover an object.
-        rect = patches.Rectangle(
-            scaled_index,
-            cell_dims[0],
-            cell_dims[1],
-            linewidth=1,
-            edgecolor="black",
-            facecolor=(255 / 255, 0 / 255, 0 / 255, 140 / 255),
-        )
-
-        ax.add_patch(rect)
+        for i in scaled_objectness_mask_indices:
+            rect_pos = patches.Rectangle(
+                i[::-1], # Flip x and y coordinates for matplotlib
+                cell_dims[0],
+                cell_dims[1],
+                linewidth=1,
+                edgecolor="black",
+                facecolor=(255 / 255, 0 / 255, 0 / 255, 140 / 255),
+            )
+            ax.add_patch(rect_pos)
+        
+        for i in scaled_loss_mask_indices:
+            rect_loss_mask = patches.Rectangle(
+                i[::-1], # Flip x and y coordinates for matplotlib
+                cell_dims[0],
+                cell_dims[1],
+                linewidth=1,
+                edgecolor="black",
+                facecolor=(255 / 255, 123 / 255, 0 / 255, 140 / 255),
+            )
+            ax.add_patch(rect_loss_mask)
 
     plt.show()
