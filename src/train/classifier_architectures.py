57a856678fd4535864948cdb4969876747abbcde
Laurens Schiefelbein <laurensschiefelbein@gmail.com>
2025-11-05 17:36:53 +0100

    add new classifier architectures

--- src/train/classifier_architectures.py
+++ src/train/classifier_architectures.py
@@ -102,6 +102,26 @@ def get_classifier(
             with_offset,
             use_batch_norm,
         )
+    if classifier_architecture == "classifier_ires_single_category_v4":
+        return _get_classifier_ires_single_category_v4(
+            patch_size,
+            channels_in,
+            n_meta,
+            n_context,
+            n_classes,
+            with_offset,
+            use_batch_norm,
+        )
+    if classifier_architecture == "classifier_ires_single_category_v5":
+        return _get_classifier_ires_single_category_v5(
+            patch_size,
+            channels_in,
+            n_meta,
+            n_context,
+            n_classes,
+            with_offset,
+            use_batch_norm,
+        )
     else:
         raise ValueError(f"Unknown classifier name: {classifier_architecture}")
 
@@ -352,6 +372,7 @@ def _get_classifier_single_category_v2(
 
     return _get_common_classifier_output(x, n_classes, with_offset, inputs)
 
+
 def _get_classifier_ires_single_category(
     patch_size: list[int],
     channels_in: int,
@@ -391,6 +412,7 @@ def _get_classifier_ires_single_category(
 
     return _get_common_classifier_output(x, n_classes, with_offset, inputs)
 
+
 def _get_classifier_ires_single_category_v2(
     patch_size: list[int],
     channels_in: int,
@@ -426,6 +448,7 @@ def _get_classifier_ires_single_category_v2(
 
     return _get_common_classifier_output(x, n_classes, with_offset, inputs)
 
+
 def _get_classifier_ires_single_category_v3(
     patch_size: list[int],
     channels_in: int,
@@ -457,6 +480,77 @@ def _get_classifier_ires_single_category_v3(
     x = tf.keras.layers.Flatten()(x)
 
     x = tf.keras.layers.Dense(24)(x)
+    x = tf.keras.layers.ReLU(6.0)(x)
+
+    return _get_common_classifier_output(x, n_classes, with_offset, inputs)
+
+
+def _get_classifier_ires_single_category_v4(
+    patch_size: list[int],
+    channels_in: int,
+    n_meta: int,
+    n_context: int,
+    n_classes: int,
+    with_offset: bool,
+    use_batch_norm: bool,
+):
+    image = tf.keras.layers.Input((*patch_size, channels_in))
+    inputs = [image]
+
+    if n_meta > 0:
+        meta = tf.keras.layers.Input((n_meta,))
+        inputs += [meta]
+
+    if n_context > 0:
+        context = tf.keras.layers.Input((n_context,))
+        inputs += [n_context]
+
+    x = image
+    if n_meta > 0:
+        x = tf.keras.layers.Concatenate()([image, meta])
+
+    x = IresBlock(8, use_batch_norm, stride=2, expansion=6)(x)
+    x = IresBlock(16, use_batch_norm, stride=2, expansion=6)(x)
+    x = IresBlock(16, use_batch_norm, stride=2, expansion=6)(x)
+
+    x = tf.keras.layers.Flatten()(x)
+
+    x = tf.keras.layers.Dense(24)(x)
+    x = tf.keras.layers.ReLU(6.0)(x)
+
+    return _get_common_classifier_output(x, n_classes, with_offset, inputs)
+
+def _get_classifier_ires_single_category_v5(
+    patch_size: list[int],
+    channels_in: int,
+    n_meta: int,
+    n_context: int,
+    n_classes: int,
+    with_offset: bool,
+    use_batch_norm: bool,
+):
+    image = tf.keras.layers.Input((*patch_size, channels_in))
+    inputs = [image]
+
+    if n_meta > 0:
+        meta = tf.keras.layers.Input((n_meta,))
+        inputs += [meta]
+
+    if n_context > 0:
+        context = tf.keras.layers.Input((n_context,))
+        inputs += [n_context]
+
+    x = image
+    if n_meta > 0:
+        x = tf.keras.layers.Concatenate()([image, meta])
+
+    x = IresBlock(8, use_batch_norm, stride=2, expansion=6)(x)
+    x = IresBlock(8, use_batch_norm, stride=1, expansion=6)(x)
+    x = IresBlock(8, use_batch_norm, stride=2, expansion=6)(x)
+
+    x = tf.keras.layers.Flatten()(x)
+
+    x = tf.keras.layers.Dense(8)(x)
     x = tf.keras.layers.ReLU(6.0)(x)
 
     return _get_common_classifier_output(x, n_classes, with_offset, inputs)
