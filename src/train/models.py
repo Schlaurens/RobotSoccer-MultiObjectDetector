5747f74730d83ce440bc869d6232346ba0381fb1
Laurens Schiefelbein <laurensschiefelbein@gmail.com>
2026-01-07 23:25:37 +0100

    different way to calculate y_true in classifier_loss

--- src/train/models.py
+++ src/train/models.py
@@ -218,12 +218,26 @@ class FullModel(tf.keras.Model):
             )
 
             # Get the indices of the cell_grid where the cell of the chosen patches point to.
-            indices_of_true_patch_coords = dataset_utils.flatten_cell_indices(
-                dataset_utils.get_cell_of_coordinate(coords_true_of_patches)
-            )  # (B, N)
-            y_true = tf.gather(
-                one_hot_mask, indices_of_true_patch_coords, batch_dims=1
-            )  # (B, N, N_O)
+            # indices_of_true_patch_coords = dataset_utils.flatten_cell_indices(
+            #     dataset_utils.get_cell_of_coordinate(coords_true_of_patches)
+            # )  # (B, N)
+            # y_true = tf.gather(
+            #     one_hot_mask, indices_of_true_patch_coords, batch_dims=1
+            # )  # (B, N, N_O)
+
+            y_true = tf.one_hot(
+                tf.cast(
+                    dataset_utils.get_groundtruth_class_of_patches(
+                        results,
+                        batch_data,
+                        padding=self.categories[object_name]["padding"],
+                        batch_dims=1,
+                    ),
+                    tf.int32,
+                ),
+                len(u_dataset.IntersectionType),
+                axis=-1,
+            )  # (B, N, num_classes)
 
             cross_entropy_batched = tf.keras.losses.CategoricalCrossentropy(
                 from_logits=False, reduction=None, name="classifier_cce"
