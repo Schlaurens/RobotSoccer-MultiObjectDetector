6a3ed3090e1388b65bbc8e2feed9013ab5f4117f
Laurens Schiefelbein <laurensschiefelbein@gmail.com>
2026-03-18 10:22:11 +0100

    feature: make model_timestamp an input argument to shell script

--- scripts/compare_results.sh
+++ scripts/compare_results.sh
@@ -1,14 +1,21 @@
 DIRECTORY="data/evaluation"
-TIMESTAMP="20260316-145655"
 DISTANCE_THRESHOLD="30"
+
+# Check if the parent directory is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <model_timestamp>"
+    exit 1
+fi
+
+timestamp="$1"
 
 echo "Comparing Predictions..."
 uv run src/evaluation/compare_predictions.py \
     --directory "$DIRECTORY" \
-    --model_timestamp "$TIMESTAMP" \
+    --model_timestamp "$timestamp" \
     --distance_threshold "$DISTANCE_THRESHOLD" \
 
 echo "Comparing Coordinate Errors..."
 uv run src/evaluation/compare_coord_errors.py \
-    --model_timestamp "$TIMESTAMP" \
+    --model_timestamp "$timestamp" \
     --directory "$DIRECTORY" \
