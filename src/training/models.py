b6c9935be6831facee5e7615c7416033a15c9e29
Laurens Schiefelbein <laurensschiefelbein@gmail.com>
2026-03-31 19:09:26 +0200

    Track new metric in training

--- src/training/models.py
+++ src/training/models.py
@@ -314,6 +314,7 @@ class FullModel(tf.keras.Model):
 
         for key in self.categories:
             result[f"encoder_bce_{key}"] = encoder_losses[key]["bce"]
+            result[f"encoder_gm_bce_{key}"] = encoder_losses[key]["gm_bce"]
             result[f"encoder_mse_{key}"] = encoder_losses[key]["mse"]
             result[f"encoder_rmse_{key}"] = encoder_losses[key]["rmse"]
             result[f"classifier_ce_{key}"] = classifier_losses[key]["ce"]
