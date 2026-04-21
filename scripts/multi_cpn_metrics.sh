52e3ca8842ca0ab2e5fb5269b1f6714d1e68deb1
Laurens Schiefelbein <laurensschiefelbein@gmail.com>
2026-04-21 11:23:01 +0200

    Make classifier evaluation possible in this script

--- scripts/multi_cpn_metrics.sh
+++ scripts/multi_cpn_metrics.sh
@@ -2,10 +2,17 @@
 
 export TF_CPP_MIN_LOG_LEVEL=2
 
-LOG_DIR="logs/fit/cpn-grayscale"
-MODEL_DIR="models/evaluation/cpn-grayscale"
+# CPN_EVAL=True
+# LOG_DIR="logs/fit/cpn-yuyv"
+# MODEL_DIR="models/evaluation/cpn-yuyv"
 
-for timestamp in "$LOG_DIR"/*/*; do
+CLASSIFIER_EVAL=True
+LOG_DIR="logs/fit/classifier-basic"
+MODEL_DIR="models/evaluation/classifier-basic"
+
+for timestamp in "$LOG_DIR"/* "$LOG_DIR"/*/*; do
     timestamp=$(basename "$timestamp")
-    uv run src/evaluation/cpn_metrics.py "$timestamp" "$LOG_DIR" "$MODEL_DIR"
+    if [[ "$timestamp" =~ ^[0-9]{8}-[0-9]{6}$ ]]; then
+        uv run src/evaluation/cpn_metrics.py --model_timestamp "$timestamp" --log_dir "$LOG_DIR" --model_dir "$MODEL_DIR" --cpn "$CPN_EVAL" --classifier "$CLASSIFIER_EVAL"
+    fi
 done
