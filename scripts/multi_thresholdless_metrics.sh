d875ab5b66cfebc50ea71cafb7575a1e428f9933
Laurens Schiefelbein <laurensschiefelbein@gmail.com>
2026-05-10 22:57:22 +0200

    adjust script

--- scripts/multi_thresholdless_metrics.sh
+++ scripts/multi_thresholdless_metrics.sh
@@ -6,13 +6,24 @@ export TF_CPP_MIN_LOG_LEVEL=2
 # LOG_DIR="logs/fit/cpn-yuyv"
 # MODEL_DIR="models/evaluation/cpn-yuyv"
 
-CLASSIFIER_EVAL=True
-LOG_DIR="logs/fit/classifier-basic"
-MODEL_DIR="models/evaluation/classifier-basic"
+CPN_EVAL=True
+LOG_DIR_BASE="logs/fit/cpn-grayscale"
+MODEL_DIR_BASE="models/evaluation/cpn-grayscale"
 
-for timestamp in "$LOG_DIR"/* "$LOG_DIR"/*/*; do
-    timestamp=$(basename "$timestamp")
-    if [[ "$timestamp" =~ ^[0-9]{8}-[0-9]{6}$ ]]; then
-        uv run src/evaluation/thresholdless_metrics.py --model_timestamp "$timestamp" --log_dir "$LOG_DIR" --model_dir "$MODEL_DIR" --cpn "$CPN_EVAL" --classifier "$CLASSIFIER_EVAL"
-    fi
+# CLASSIFIER_EVAL=True
+# LOG_DIR_BASE="logs/fit/classifier-basic/"
+# MODEL_DIR_BASE="models/evaluation/classifier-basic"
+
+NUM_RUNS=3
+
+for run in $(seq 1 $NUM_RUNS); do
+    LOG_DIR="${LOG_DIR_BASE}/run_${run}"
+    MODEL_DIR="${MODEL_DIR_BASE}/run_${run}"
+
+    for timestamp in "$LOG_DIR"/*/*; do
+        timestamp=$(basename "$timestamp")
+        if [[ "$timestamp" =~ ^[0-9]{8}-[0-9]{6}$ ]]; then
+            uv run src/evaluation/thresholdless_metrics.py --model_timestamp "$timestamp" --log_dir "$LOG_DIR" --model_dir "$MODEL_DIR" --cpn "$CPN_EVAL" --classifier "$CLASSIFIER_EVAL"
+        fi
+    done
 done
