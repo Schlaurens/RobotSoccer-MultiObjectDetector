836291e0fd13f1689a1d3101c0dba49afa1ffb3e
Laurens Schiefelbein <laurens@uni-bremen.de>
2026-08-07 14:04:38 +0200

    remove some tests: threshold modes dont exist anymore

--- tests/util/test_metrics.py
+++ tests/util/test_metrics.py
@@ -4,7 +4,7 @@ from src.util import metrics as u_metrics
 
 
 class TestGetThresholdingMask:
-    def testBaseCaseLogicalAnd(self):
+    def testBaseCase(self):
         classifier_threshold = 0.8
         encoder_threshold = 0.1
 
@@ -13,26 +13,12 @@ class TestGetThresholdingMask:
 
         expected = tf.cast(tf.constant([[1, 1, 0], [0, 0, 0]]), tf.bool)
         results = u_metrics.get_thresholding_mask(
-            classifier_preds, classifier_threshold, encoder_preds, encoder_threshold, "logical_and"
+            classifier_preds, classifier_threshold, encoder_preds, encoder_threshold
         )
 
         assert tf.reduce_all(expected == results)
 
-    def testBaseCaseAdditive(self):
-        classifier_threshold = 0.8
-        encoder_threshold = 0.1
-
-        encoder_preds = tf.constant([[0.1, 0.4, 0.04], [0.9, 1, 0]], tf.float32)  # (2, 3)
-        classifier_preds = tf.constant([[0.9, 0.8, 1], [0.3, 0, 0]], tf.float32)  # (2, 3)
-
-        expected = tf.cast(tf.constant([[1, 1, 1], [1, 1, 0]]), tf.bool)
-        results = u_metrics.get_thresholding_mask(
-            classifier_preds, classifier_threshold, encoder_preds, encoder_threshold, "additive"
-        )
-
-        assert tf.reduce_all(expected == results)
-
-    def testFlatInputLogicalAnd(self):
+    def testFlatInput(self):
         classifier_threshold = 0.8
         encoder_threshold = 0.1
 
@@ -41,26 +27,12 @@ class TestGetThresholdingMask:
 
         expected = tf.cast(tf.constant([1, 1, 0, 0, 0, 0]), tf.bool)
         results = u_metrics.get_thresholding_mask(
-            classifier_preds, classifier_threshold, encoder_preds, encoder_threshold, "logical_and"
+            classifier_preds, classifier_threshold, encoder_preds, encoder_threshold
         )
 
         assert tf.reduce_all(expected == results)
 
-    def testFlatInputAdditive(self):
-        classifier_threshold = 0.8
-        encoder_threshold = 0.1
-
-        encoder_preds = tf.constant([0.1, 0.4, 0.04, 0.9, 1, 0], tf.float32)  # (6)
-        classifier_preds = tf.constant([0.9, 0.8, 1, 0.3, 0, 0], tf.float32)  # (6)
-
-        expected = tf.cast(tf.constant([1, 1, 1, 1, 1, 0]), tf.bool)
-        results = u_metrics.get_thresholding_mask(
-            classifier_preds, classifier_threshold, encoder_preds, encoder_threshold, "additive"
-        )
-
-        assert tf.reduce_all(expected == results)
-
-    def testHighDimensionsLogicalAnd(self):
+    def testHighDimensions(self):
         classifier_threshold = 0.8
         encoder_threshold = 0.1
 
@@ -69,21 +41,7 @@ class TestGetThresholdingMask:
 
         expected = tf.cast(tf.constant([[[[[1], [1], [0]]], [[[0], [0], [0]]]]]), tf.bool)
         results = u_metrics.get_thresholding_mask(
-            classifier_preds, classifier_threshold, encoder_preds, encoder_threshold, "logical_and"
-        )
-
-        assert tf.reduce_all(expected == results)
-
-    def testHighDimensionsAdditive(self):
-        classifier_threshold = 0.8
-        encoder_threshold = 0.1
-
-        encoder_preds = tf.constant([[[[[0.1], [0.4], [0.04]]], [[[0.9], [1], [0]]]]], tf.float32)
-        classifier_preds = tf.constant([[[[[0.9], [0.8], [1]]], [[[0.3], [0], [0]]]]], tf.float32)
-
-        expected = tf.cast(tf.constant([[[[[1], [1], [1]]], [[[1], [1], [0]]]]]), tf.bool)
-        results = u_metrics.get_thresholding_mask(
-            classifier_preds, classifier_threshold, encoder_preds, encoder_threshold, "additive"
+            classifier_preds, classifier_threshold, encoder_preds, encoder_threshold
         )
 
         assert tf.reduce_all(expected == results)
